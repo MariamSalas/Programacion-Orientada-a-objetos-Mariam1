044644829971d56028751477fafd9b6f070aa21b
DSV <mariamsalas81@gmail.com>
2023-02-07 09:32:09 -0600

    Hola

---
+++ src/reto3/proceso/Conversor.java
@@ -0,0 +1,18 @@
+package reto3.proceso;
+/**
+ * Esta clase tiene metodos para convertir numero de decimal a binario
+ */
+public class Conversor {
+    /**
+     * Este metodo tiene un string que contiene un numero base
+     * decimal y el mismo lo convierte en uno base binario
+     *
+     * @param deciamal convertir a base deciamal
+     * @retur numero en base binario
+     */
+    public static String conversor (String deciamal){
+        return null;
+        }
+    }
+
+}
